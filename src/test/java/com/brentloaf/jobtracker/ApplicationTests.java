6fcf1bbf94be42e5d0948b798c142142a23d1f15
BrentLoaf <kcoldoowtnerbg@gmail.com>
2026-03-16 23:08:04 -0700

    Initial commit

---
+++ src/test/java/com/brentloaf/jobtracker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brentloaf.jobtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
